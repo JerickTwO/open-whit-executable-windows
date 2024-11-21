b026ed12832c107508cb8bec52e1ca576f2a945f
JerickTwO <jerickjjtm774@gmail.com>
2024-11-21 18:31:30 -0500

    first commit

---
+++ stack.bat
@@ -0,0 +1,3 @@
+start /d "C:\Program Files\JetBrains\IntelliJ IDEA 2024.1.4\bin" idea64.exe
+start /d "C:\Users\JERICK\AppData\Local\Programs\Microsoft VS Code" Code.exe
+start /d "C:\laragon" laragon.exe
